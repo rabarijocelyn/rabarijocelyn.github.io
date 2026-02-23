e28cc3cac7b901c3399d13fbf88255082c4a2282
RABARIAMALALA Jocelyn <114258769+Wazor999@users.noreply.github.com>
2026-02-23 11:08:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon Portfolio</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Bienvenue sur mon portfolio</h1>
+    <p>Je suis [Ton Nom]</p>
+
+    <h2>Mes projets</h2>
+    <ul>
+        <li>Projet 1</li>
+        <li>Projet 2</li>
+        <li>Projet 3</li>
+    </ul>
+
+    <h2>Contact</h2>
+    <p>Email : wazorjoslyn@gmail.com</p>
+</body>
+</html>
